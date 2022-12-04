afd2ae744146d1a5e0d3c96e12f56bd1f00e55d8
Fotima <fotimasayfullayeva6@gmail.com>
2022-12-04 15:40:19 +0300

    Solve exercises

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿//2
+// Console.Write("Enter first number: ");
+// int numb1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter second number: ");
+// int numb2 = Convert.ToInt32(Console.ReadLine());
+
+// if (numb1 > numb2)
+// {
+//     Console.WriteLine("max = " + numb1);
+// }
+// if (numb1 < numb2)
+// {
+//     Console.WriteLine("max = " + numb2);
+// }
+// else
+// {
+//     Console.WriteLine("They are equal!");
+// }
+
+//4
+// Console.Write("Enter first number: ");
+// int numb1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter second number: ");
+// int numb2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter third number: ");
+// int numb3 = Convert.ToInt32(Console.ReadLine());
+// if (numb1 > numb2) {
+//     Console.WriteLine("max = " + numb1);
+// }
+// else if (numb2 > numb3) {
+//     Console.WriteLine("max = " + numb2);
+// }
+// else if (numb1 < numb3) {
+//     Console.WriteLine("max = " + numb3);
+// }
+// else{
+//     Console.WriteLine("Mistake!");
+// }
+
+//6
+// Console.Write("Enter a number: ");
+// int numb1 = Convert.ToInt32(Console.ReadLine());
+// if (numb1 % 2 == 0) {
+//     Console.WriteLine("Да!");
+// }
+// else {
+//     Console.WriteLine("Нет!");
+// }
+
+//8
+// Console.Write("Enter a number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int t = 0;
+
+// while (t+2 <= number){
+//     t = t + 2;
+//     Console.Write(number + " = "+ t + ", ");
+// }
